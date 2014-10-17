01c4b684edc125af6701fa56b10c80f524a69591
paul devenport <pauldev@ldschurch.org>
2014-10-17 14:57:59 -0600

    db update

---
+++ documentation/script-2014-10-20.sql
@@ -0,0 +1,32 @@
+dummy-utf8;
+
+sET DEFINE OFF;
+set escape on;
+alter session set CURRENT_SCHEMA = bookscan;
+ 
+ alter TABLE book add pull_date timestamp;
+
+
+
+DROP TABLE bookviewingstats CASCADE CONSTRAINTS;
+
+CREATE TABLE bookviewingstats
+(
+PID varchar2(255 CHAR) NOT NULL, 
+num_of_views NUMBER(11,0), 
+title varchar2(2047 CHAR)  , 
+access_rights varchar2(255 CHAR), 
+collection varchar2(2047 char),
+tn varchar2(255 CHAR) NOT NULL,
+Publisher varchar2(255 CHAR),
+owning_institution varchar2(255 CHAR),  
+ie_url varchar2(255 CHAR),
+report_date date,
+
+
+ CONSTRAINT pk1 PRIMARY KEY 
+  (
+    tn, report_date
+  )
+  ENABLE 
+); 
