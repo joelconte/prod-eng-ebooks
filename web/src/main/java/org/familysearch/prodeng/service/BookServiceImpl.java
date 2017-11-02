8923c5d1a5f47f410e56c9ff860e52481927c16b
pauldev <pauldevibm@yahoo.com>
2017-11-02 13:14:02 -0600

    IA returning duplicate identifiers checking logic.  Fetch REST call with 1000 row chuncks with max 5000.

--- web/src/main/java/org/familysearch/prodeng/service/BookServiceImpl.java
+++ web/src/main/java/org/familysearch/prodeng/service/BookServiceImpl.java
@@ -764,7 +764,9 @@ public class BookServiceImpl extends NamedParameterJdbcDaoSupport implements Boo
 			paramsArray[iRow++] = params; 
 			params = new HashMap<String, Object>();
 		}
+		
 		getNamedParameterJdbcTemplate().batchUpdate(sql, paramsArray);
+
 	}
 
 	@Override
